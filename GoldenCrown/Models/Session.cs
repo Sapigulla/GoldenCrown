965f4c576db8c0d6d336966003003f2b709587fc
sapigulla <sapi@usr.tech>
2026-09-13 18:38:20 +0300

    Создаём классы и модели

---
+++ GoldenCrown/Models/Session.cs
@@ -0,0 +1,10 @@
+﻿namespace GoldenCrown.Models
+{
+    public class Session
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+
+    }
+}
