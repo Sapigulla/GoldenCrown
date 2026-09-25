ba33e05b82d4ad590d11d8ee35ba790745dcf92f
sapigulla <sapi@usr.tech>
2026-09-25 16:08:43 +0300

    Создание DTO

---
+++ GoldenCrown/Dtos/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace GoldenCrown.Dtos
+{
+    public class LoginRequest
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
